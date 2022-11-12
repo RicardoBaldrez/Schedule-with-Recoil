3da9f156ef978355f897fd48f12e5816a7ccce5d
Ricardo Baldrez <ricardo.baldrez@gmail.com>
2022-11-12 15:02:06 -0300

    refactor: 💡 Eslint inclusion

--- src/components/Filtro/index.tsx
+++ src/components/Filtro/index.tsx
@@ -1,34 +1,36 @@
 import React, { useState } from 'react';
+
 import style from './Filtro.module.scss';
 
-const Filtro: React.FC<{ aoFiltroAplicado: (data: Date | null) => void }> = ({ aoFiltroAplicado }) => {
-  
-  const [data, setData] = useState('')
-  
-  const submeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
-    evento.preventDefault()
-    if (!data) {
-      aoFiltroAplicado(null)
-      return
-    }
-    aoFiltroAplicado(new Date(data))
-  }
+const Filtro: React.FC<{ aoFiltroAplicado: (data: Date | null) => void }> = ({
+	aoFiltroAplicado
+}) => {
+	const [data, setData] = useState('');
 
-  return (<form className={style.Filtro} onSubmit={submeterForm}>
-    <h3 className={style.titulo}>Filtrar por data</h3>
-    <input 
-      type="date" 
-      name="data"
-      className={style.input}
-      onChange={evento => setData(evento.target.value)} 
-      placeholder="Por data"
-      value={data} />
+	const submeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+		evento.preventDefault();
+		if (!data) {
+			aoFiltroAplicado(null);
+			return;
+		}
+		aoFiltroAplicado(new Date(data));
+	};
 
-    <button className={style.botao}>
-      Filtrar
-    </button>
+	return (
+		<form className={style.Filtro} onSubmit={submeterForm}>
+			<h3 className={style.titulo}>Filtrar por data</h3>
+			<input
+				type="date"
+				name="data"
+				className={style.input}
+				onChange={(evento) => setData(evento.target.value)}
+				placeholder="Por data"
+				value={data}
+			/>
 
-  </form>)
-}
+			<button className={style.botao}>Filtrar</button>
+		</form>
+	);
+};
 
-export default Filtro
+export default Filtro;
